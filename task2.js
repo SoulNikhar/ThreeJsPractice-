c04bd5b57060e070531a7509790d2eee31775f18
Nikhar Behera <111144948+SoulNikhar@users.noreply.github.com>
2025-03-10 11:03:26 +0530

    Update task2.js

--- task2.js
+++ task2.js
@@ -1,4 +1,3 @@
-
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
@@ -26,10 +25,12 @@ const baseMaterial = new THREE.MeshStandardMaterial({
   wireframe: false,
 });
 const baseBox = new THREE.Mesh(baseGeometry, baseMaterial);
+baseBox.receiveShadow = true;
 scene.add(baseBox);
 
-// const ambientLight = new THREE.AmbientLight(0x404040, 2);
-// scene.add(ambientLight);
+
+const ambientLight = new THREE.AmbientLight(0x404040, 2);
+scene.add(ambientLight);
 
 function removePreviousObjects() {
   sceneObjects.forEach((e) => {
@@ -40,18 +41,22 @@ function removePreviousObjects() {
   sceneObjects = [];
 }
 
+function addShadowsToLight(light) {
+  light.castShadow = true;
+  light.shadow.mapSize.width = 1024;
+  light.shadow.mapSize.height = 1024;
+  light.shadow.camera.near = 0.5;
+  light.shadow.camera.far = 50;
+}
+
+
 function addPointLightsAndCubes() {
   removePreviousObjects();
-
   const light1 = new THREE.PointLight(0xffffff, 50);
-  light1.position.set(0, 0, 0);
+  light1.position.set(0, 5, 0);
+  addShadowsToLight(light1);
   scene.add(light1);
   sceneObjects.push(light1);
-
-  const light2 = new THREE.DirectionalLight(0xffaa00, 1, 10);
-  light2.position.set(3, 10, 3);
-  scene.add(light2);
-  sceneObjects.push(light2);
 
   const geometry = new THREE.BoxGeometry(1, 1, 1);
   const material1 = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
@@ -59,27 +64,24 @@ function addPointLightsAndCubes() {
 
   const cube1 = new THREE.Mesh(geometry, material1);
   cube1.position.set(-1, 1, 0);
+  cube1.castShadow = true;
   scene.add(cube1);
   sceneObjects.push(cube1);
 
   const cube2 = new THREE.Mesh(geometry, material2);
   cube2.position.set(1, 1, 0);
+  cube2.castShadow = true;
   scene.add(cube2);
   sceneObjects.push(cube2);
 }
 
 function addDirectionalLightsAndSpheres() {
   removePreviousObjects();
-
   const light1 = new THREE.DirectionalLight(0xff0000, 1);
   light1.position.set(1, 3, 3);
+  addShadowsToLight(light1);
   scene.add(light1);
   sceneObjects.push(light1);
-
-  const light2 = new THREE.DirectionalLight(0x0fffff, 1);
-  light2.position.set(0, 2, 0);
-  scene.add(light2);
-  sceneObjects.push(light2);
 
   const geometry = new THREE.SphereGeometry(0.5, 32, 32);
   const material1 = new THREE.MeshNormalMaterial();
@@ -87,43 +89,62 @@ function addDirectionalLightsAndSpheres() {
 
   const sphere1 = new THREE.Mesh(geometry, material1);
   sphere1.position.set(-1, 1, 1);
+  sphere1.castShadow = true;
   scene.add(sphere1);
   sceneObjects.push(sphere1);
 
   const sphere2 = new THREE.Mesh(geometry, material2);
   sphere2.position.set(1, 1, -1);
+  sphere2.castShadow = true;
   scene.add(sphere2);
   sceneObjects.push(sphere2);
 }
 
+
 function addSpotLightsAndCones() {
   removePreviousObjects();
 
+  // Set up the first spotlight
   const light1 = new THREE.SpotLight(0x0000ff, 5);
   light1.position.set(2, 4, 2);
+  light1.castShadow = true; // Enable shadow for the light
   scene.add(light1);
   sceneObjects.push(light1);
 
+  // Set up the second spotlight
   const light2 = new THREE.SpotLight(0xff00ff, 10);
   light2.position.set(-2, 4, -2);
+  light2.castShadow = true; // Enable shadow for the light
   scene.add(light2);
   sceneObjects.push(light2);
 
+  // Set up geometry and materials for the cones
   const geometry = new THREE.ConeGeometry(0.5, 1, 32);
   const material1 = new THREE.MeshStandardMaterial({ color: 0x0000ff });
   const material2 = new THREE.MeshStandardMaterial({ color: 0xff00ff });
 
+  // Create and position the first cone
   const cone1 = new THREE.Mesh(geometry, material1);
   cone1.position.set(-1, 0.5, 1);
+  cone1.castShadow = true; // Enable shadow for the cone
+  cone1.receiveShadow = true; // Enable receiving shadow for the cone
   scene.add(cone1);
   sceneObjects.push(cone1);
 
+  // Create and position the second cone
   const cone2 = new THREE.Mesh(geometry, material2);
   cone2.position.set(1, 0.5, -1);
+  cone2.castShadow = true; // Enable shadow for the cone
+  cone2.receiveShadow = true; // Enable receiving shadow for the cone
   scene.add(cone2);
   sceneObjects.push(cone2);
+
+  // Ensure the scene renderer has shadows enabled
+  renderer.shadowMap.enabled = true;
+  renderer.shadowMap.type = THREE.PCFSoftShadowMap; // Use softer shadows
 }
 
+ 
 function addHemisphereLightAndTorusKnot() {
   removePreviousObjects();
 
@@ -133,33 +154,64 @@ function addHemisphereLightAndTorusKnot() {
     wireframe: false,
   });
   const torusKnot = new THREE.Mesh(geometry, material);
-
-  const light = new THREE.HemisphereLight(0x0000ff, 0xff0000, 2);
-  light.position.set(0, 5, 0);
-  scene.add(light);
-  sceneObjects.push(light);
-
   torusKnot.position.set(0, 2, 0);
+  torusKnot.castShadow = true;
+  torusKnot.receiveShadow = true;
   scene.add(torusKnot);
   sceneObjects.push(torusKnot);
+
+  const hemiLight = new THREE.HemisphereLight(0x0000ff, 0xff0000, 2);
+  hemiLight.position.set(0, 5, 0);
+  scene.add(hemiLight);
+  sceneObjects.push(hemiLight);
+
+  // Add a DirectionalLight for shadow casting
+  const dirLight = new THREE.DirectionalLight(0xffffff, 2);
+  dirLight.position.set(2, 5, 2);
+  dirLight.castShadow = true;
+  dirLight.shadow.mapSize.width = 1024;
+  dirLight.shadow.mapSize.height = 1024;
+  dirLight.shadow.camera.near = 0.5;
+  dirLight.shadow.camera.far = 10;
+  scene.add(dirLight);
+  sceneObjects.push(dirLight);
+
+  // Enable shadows in the renderer
+  renderer.shadowMap.enabled = true;
+  renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 }
 
 function addRectAreaLightAndCylinder() {
   removePreviousObjects();
 
-  const light = new THREE.RectAreaLight(0xffffff, 5, 7, 5);
-  light.position.set(0, 4, 2);
-  scene.add(light);
-  sceneObjects.push(light);
-
   const geometry = new THREE.CylinderGeometry(0.5, 0.5, 1.5, 32);
   const material = new THREE.MeshStandardMaterial({ color: 0x008080 });
   const cylinder = new THREE.Mesh(geometry, material);
-
   cylinder.position.set(0, 1.5, 0);
+  cylinder.castShadow = true;
+  cylinder.receiveShadow = true;
   scene.add(cylinder);
-
   sceneObjects.push(cylinder);
+
+  const rectLight = new THREE.RectAreaLight(0xffffff, 5, 7, 5);
+  rectLight.position.set(0, 4, 2);
+  scene.add(rectLight);
+  sceneObjects.push(rectLight);
+
+  // Add a SpotLight for shadow casting
+  const spotLight = new THREE.SpotLight(0xffffff, 3);
+  spotLight.position.set(2, 5, 2);
+  spotLight.angle = Math.PI / 6;
+  spotLight.penumbra = 0.5;
+  spotLight.castShadow = true;
+  spotLight.shadow.mapSize.width = 1024;
+  spotLight.shadow.mapSize.height = 1024;
+  scene.add(spotLight);
+  sceneObjects.push(spotLight);
+
+  // Enable shadows in the renderer
+  renderer.shadowMap.enabled = true;
+  renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 }
 
 function addDirectionalLightAndTube() {
@@ -167,6 +219,7 @@ function addDirectionalLightAndTube() {
 
   const light = new THREE.DirectionalLight(0x008000, 5);
   light.position.set(0, 3, 8);
+  light.castShadow = true; // Enable shadows
   scene.add(light);
   sceneObjects.push(light);
 
@@ -175,13 +228,19 @@ function addDirectionalLightAndTube() {
     new THREE.Vector3(1, 1, 1),
     new THREE.Vector3(1, 1, -1),
     new THREE.Vector3(-1, 0, -1),
-  ]);
+    new THREE.Vector3(-1, 0 , 1)
+  ]); 
   const geometry = new THREE.TubeGeometry(path, 20, 0.2, 8, false);
   const material = new THREE.MeshStandardMaterial({ color: 0x000f0ff });
   const tube = new THREE.Mesh(geometry, material);
   tube.position.set(0, 1.5, 0);
+  tube.castShadow = true;
+  tube.receiveShadow = true;
   scene.add(tube);
   sceneObjects.push(tube);
+
+  renderer.shadowMap.enabled = true;
+  renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 }
 
 function addSpotLightAndTetrahedron() {
@@ -189,14 +248,16 @@ function addSpotLightAndTetrahedron() {
 
   const light = new THREE.SpotLight(0xff0000, 10);
   light.position.set(0, 4, 0);
+  light.castShadow = true;
   scene.add(light);
   sceneObjects.push(light);
 
   const geometry = new THREE.TetrahedronGeometry(0.7);
   const material = new THREE.MeshPhongMaterial({ color: 0xffffff });
   const tetrahedron = new THREE.Mesh(geometry, material);
-
   tetrahedron.position.set(0, 0.5, 0);
+  tetrahedron.castShadow = true;
+  tetrahedron.receiveShadow = true;
   scene.add(tetrahedron);
   sceneObjects.push(tetrahedron);
 }
@@ -206,6 +267,7 @@ function addPointLightAndSphere() {
 
   const light = new THREE.PointLight(0x00ffff, 70);
   light.position.set(0, 2, 2);
+  light.castShadow = true;
   scene.add(light);
   sceneObjects.push(light);
 
@@ -213,6 +275,8 @@ function addPointLightAndSphere() {
   const material = new THREE.MeshStandardMaterial({ color: 0xff4500 });
   const sphere = new THREE.Mesh(geometry, material);
   sphere.position.set(0, 1, 0);
+  sphere.castShadow = true;
+  sphere.receiveShadow = true;
   scene.add(sphere);
   sceneObjects.push(sphere);
 }
@@ -224,10 +288,19 @@ function addAmbientLightAndDodecahedron() {
   scene.add(light);
   sceneObjects.push(light);
 
+  // Add a directional light for shadows (since AmbientLight does not cast shadows)
+  const shadowLight = new THREE.DirectionalLight(0xffffff, 5);
+  shadowLight.position.set(2, 5, 2);
+  shadowLight.castShadow = true;
+  scene.add(shadowLight);
+  sceneObjects.push(shadowLight);
+
   const geometry = new THREE.DodecahedronGeometry(0.7);
-  const material = new THREE.MeshStandardMaterial({ color: 0xffff00 , wireframe:false});
+  const material = new THREE.MeshStandardMaterial({ color: 0xffff00, wireframe: false });
   const dodecahedron = new THREE.Mesh(geometry, material);
-  dodecahedron.position.set(0,1,0);
+  dodecahedron.position.set(0, 1, 0);
+  dodecahedron.castShadow = true;
+  dodecahedron.receiveShadow = true;
   scene.add(dodecahedron);
   sceneObjects.push(dodecahedron);
 }
@@ -237,35 +310,45 @@ function addSpotLightAndOctahedron() {
 
   const light = new THREE.SpotLight(0x0000ff, 10);
   light.position.set(2, 3, 0);
+  light.castShadow = true;
   scene.add(light);
   sceneObjects.push(light);
 
   const light2 = new THREE.SpotLight(0x0000ff, 10);
   light2.position.set(0, 3, 0);
+  light2.castShadow = true;
   scene.add(light2);
   sceneObjects.push(light2);
 
   const geometry = new THREE.OctahedronGeometry(0.6);
   const material = new THREE.MeshPhongMaterial({ color: 0x00ffff });
   const octahedron = new THREE.Mesh(geometry, material);
-
+  octahedron.castShadow = true;
+  octahedron.receiveShadow = true;
   scene.add(octahedron);
-  
   sceneObjects.push(octahedron);
 }
 
 function addRectAreaLightAndTorus() {
   removePreviousObjects();
 
-  const light = new THREE.RectAreaLight(0xffffff, 20 );
-  light.position.set(0,1,0);
+  const light = new THREE.RectAreaLight(0xffffff, 20);
+  light.position.set(0, 1, 0);
   scene.add(light);
   sceneObjects.push(light);
+
+  // Add a SpotLight for shadows (since RectAreaLight does not cast shadows)
+  const shadowLight = new THREE.SpotLight(0xffffff, 5);
+  shadowLight.position.set(3, 3, 3);
+  shadowLight.castShadow = true;
+  scene.add(shadowLight);
+  sceneObjects.push(shadowLight);
 
   const geometry = new THREE.TorusGeometry(0.6, 0.2, 16, 100);
   const material = new THREE.MeshPhysicalMaterial({ color: 0x0000ff });
   const torus = new THREE.Mesh(geometry, material);
-
+  torus.castShadow = true;
+  torus.receiveShadow = true;
   scene.add(torus);
   sceneObjects.push(torus);
 }
